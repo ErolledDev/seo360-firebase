51055a5dff824ba7eed4a19d39db2e2968cd8277
Cedrick A.k.a ErolledDev <107593932+ErolledDev@users.noreply.github.com>
2025-06-23 23:15:06 +0800

    Update SimpleHeader.tsx

--- components/SimpleHeader.tsx
+++ components/SimpleHeader.tsx
@@ -9,7 +9,7 @@ export default function SimpleHeader() {
         <div className="flex justify-center items-center py-4">
           <Link href="/" className="flex items-center space-x-3 group">
             <div>
-              <h1 className="text-xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
+              <h1 className="text-3xl font-bold bg-gradient-to-r from-blue-600 to-indigo-600 bg-clip-text text-transparent">
                 SEO360
               </h1>
             </div>
